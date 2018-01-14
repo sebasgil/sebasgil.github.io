ed8a872e1679aa08c616dd54830a1560803c5e5d
Sebastián Gil <sebastian.higherlearning@gmail.com>
2018-01-13 22:22:19 -0800

    Personalize jumbotron

--- resources/js/contact.js
+++ resources/js/contact.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  $('#characterLeft').text('140 characters left');
+  $('#characterLeft').text('200 characters left');
   $('#message').keydown(function() {
-    var max = 140;
+    var max = 200;
     var len = $(this).val().length;
     if (len >= max) {
       $('#characterLeft').text('You have reached the message limit.');
